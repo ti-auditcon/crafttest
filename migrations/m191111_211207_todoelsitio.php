f7dcbade2de8eb34971564450b1438d0f789ec33
Juan <jberrioscifuentes@gmail.com>
2019-11-11 18:13:00 -0300

    migration?

---
+++ migrations/m191111_211207_todoelsitio.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace craft\contentmigrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m191111_211207_todoelsitio migration.
+ */
+class m191111_211207_todoelsitio extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        // Place migration code here...
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m191111_211207_todoelsitio cannot be reverted.\n";
+        return false;
+    }
+}
